e8a99fe039c86a28da04f4f3ff769a65b0512487
lei.s@yougou.com <lei.s@szsknb198.yougou.com>
2017-05-16 09:33:52 +0800

    ThreadPool小例子

--- src/main/java/com/sully/executorservice/TestFixedThreadPool.java
+++ src/main/java/com/sully/executorservice/TestFixedThreadPool.java
@@ -10,12 +10,15 @@ import java.util.concurrent.Executors;
  */
 public class TestFixedThreadPool {
     public static void main(String[] args) {
+
         ExecutorService executorService = Executors.newFixedThreadPool(2);
+
         Thread t1 = new MyThread("t1");
         Thread t2 = new MyThread("t2");
         Thread t3 = new MyThread("t3");
         Thread t4 = new MyThread("t4");
         Thread t5 = new MyThread("t5");
+
         executorService.execute(t1);
         executorService.execute(t2);
         executorService.execute(t3);
